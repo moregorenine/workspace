7e1aa6411b2325fd57913696c245fa86ee768ea7
moregorenine <moregorenine@moregorenine-PC>
2019-04-18 00:20:03 +0900

    home3 init

---
+++ home3/src/main/java/com/w4springrain/home3/Home3Application.java
@@ -0,0 +1,13 @@
+package com.w4springrain.home3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Home3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Home3Application.class, args);
+	}
+
+}
---
+++ home3/src/test/java/com/w4springrain/home3/Home3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.w4springrain.home3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Home3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
